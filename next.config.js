c70358114966ad43f815bd2aaff2b55bc9201081
Avinash R <avinash.ravichandran02@gmail.com>
2025-04-20 00:21:15 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
-const isGithubPages = process.env.NODE_ENV === 'production';
-const repo = 'portfolio-final'; // Replace this with your actual GitHub repo name
-
 const nextConfig = {
-  output: 'export', // Exports the site as a static site
-  basePath: isGithubPages ? `/${repo}` : '', // Set base path for GitHub Pages
-  assetPrefix: isGithubPages ? `/${repo}/` : '', // Set asset prefix for GitHub Pages
+  output: 'export',
   images: {
-    unoptimized: true, // Disable image optimization for static export
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
